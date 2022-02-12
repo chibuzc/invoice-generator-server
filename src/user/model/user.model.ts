148d197c79e557ede449503164f6fcdb7950e13f
Ibrahim Abubakar <ikadismile@gmail.com>
2022-02-12 18:40:08 +0000

    added env and typeorm

---
+++ src/user/model/user.model.ts
@@ -0,0 +1,44 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@ObjectType()
+export class UserModel {
+
+  @Field(() => Int)
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Field()
+  @Column({ nullable: false })
+  businessName: string;
+
+  @Field()
+  @Column({ nullable: false })
+  email: string;
+
+  @Field()
+  @Column({ nullable: false })
+  password: string;
+
+  @Field()
+  @Column({ nullable: false })
+  address: string;
+
+  @Field()
+  @Column({ nullable: false })
+  logo: string;
+
+  @Field()
+  @Column({ nullable: false })
+  phoneNumber: string;
+
+  @Field()
+  @Column()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field()
+  @Column()
+  @UpdateDateColumn()
+  updatedAt: Date
+}
